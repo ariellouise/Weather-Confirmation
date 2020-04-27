0d0418e9c209e5ae9e0fabc8ac7fa22ee52aca59
ariellouise <60407963+ariellouise@users.noreply.github.com>
2020-04-27 08:54:51 -0400

    modified scraper

---
+++ lib/scraper.rb
@@ -0,0 +1,17 @@
+class CNYWeatherScraper::CNYWeatherScraper
+
+#https://www.accuweather.com/en/us/rome/13440/weather-forecast/334623
+
+base_url = "https://www.accuweather.com/en/us/rome/13440/weather-forecast/334623?day="
+
+def self.scrape_today
+    doc = Nokogiri::HTML(open("https://www.accuweather.com/en/us/rome/13440/weather-forecast/334623?day=1"))
+      name = doc.search().text
+      high = doc.search().text
+      low = doc.search().text
+      description = doc.search().text
+      tonight = doc.search().text
+      CNYWeatherConfirmation::Forecast.new(name, high, low, description, tonight)
+
+
+      
