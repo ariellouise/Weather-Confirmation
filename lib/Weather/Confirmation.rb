a5da1491aad0f1e296edb8eceab49a327fd511e3
ariel murphy <arielladders@gmail.com>
2020-04-27 02:34:21 +0000

    first commit

---
+++ lib/Weather/Confirmation.rb
@@ -0,0 +1,8 @@
+require "Weather/Confirmation/version"
+
+module Weather
+  module Confirmation
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/Weather/Confirmation/version.rb
@@ -0,0 +1,5 @@
+module Weather
+  module Confirmation
+    VERSION = "0.1.0"
+  end
+end
